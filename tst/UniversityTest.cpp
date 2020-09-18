316b7b2ace1f7a22baacb22ba6a2ffa8bed6e6cf
fabio0610 <fabio0610@hotmail.com>
2020-09-17 23:41:22 -0600

    creacion del test

---
+++ tst/UniversityTest.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Fabio Villalobos on 17/9/2020.
+//
+#include "University.h"
+#include "Person.h"
+#include "Professor.h"
+#include "Administrative.h"
+#include "gtest/gtest.h"
+
+TEST(UniversityTestSuite, VerifyUniversityProcess){
+
+}
